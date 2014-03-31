ad61585665514867888f2d1421ed88064d0f02d6
neha <nhchdhr@yahoo.co.in>
2014-03-31 06:35:28 -0400

    Committed by Neha Choudhari on Monday  April 1st

---
+++ Mouseville/DatePickerViewController.h
@@ -0,0 +1,20 @@
+//
+//  DatePickerViewController.h
+//  Mouseville
+//
+//  Created by Abhang Sonawane on 3/31/14.
+//  Copyright (c) 2014 CapstoneProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol DatePickerDelegate
+-(void)didClickDatePicker:(NSString*) string;
+@end
+
+@interface DatePickerViewController : UIViewController
+@property (nonatomic, assign) id<DatePickerDelegate> delegate;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+- (IBAction)datePicked:(UIDatePicker *)sender;
+
+@end
