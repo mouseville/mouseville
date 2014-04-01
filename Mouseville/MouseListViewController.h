9a9639de6195def0d0c46ab83bb309caa8114354
Curtis <wilson.1339@osu.edu>
2014-04-01 06:31:06 -0700

    started messing with the mouse list in the cage view

---
+++ Mouseville/MouseListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MouseListViewController.h
+//  Mouseville
+//
+//  Created by shnee on 4/1/14.
+//  Copyright (c) 2014 CapstoneProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MouseListViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+
+@end
---
+++ Mouseville/MouseListViewController.m
@@ -0,0 +1,50 @@
+//
+//  MouseListViewController.m
+//  Mouseville
+//
+//  Created by shnee on 4/1/14.
+//  Copyright (c) 2014 CapstoneProject. All rights reserved.
+//
+
+#import "MouseListViewController.h"
+
+@interface MouseListViewController ()
+
+@end
+
+@implementation MouseListViewController
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"mouse"];
+    
+    cell.textLabel.text = @"Mouse";
+    
+    return cell;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
