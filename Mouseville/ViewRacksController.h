3243043d1eff7d9a1efc9a0eb18e2f36c4b6ae58
nayan <nayankhodke@gmail.com>
2014-04-02 15:42:48 -0700

    self commmit

---
+++ Mouseville/ViewRacksController.h
@@ -0,0 +1,13 @@
+//
+//  ViewRacksController.h
+//  Mouseville
+//
+//  Created by nayan on 4/2/14.
+//  Copyright (c) 2014 CapstoneProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewRacksController : UIViewController
+
+@end
---
+++ Mouseville/ViewRacksController.m
@@ -0,0 +1,38 @@
+//
+//  ViewRacksController.m
+//  Mouseville
+//
+//  Created by nayan on 4/2/14.
+//  Copyright (c) 2014 CapstoneProject. All rights reserved.
+//
+
+#import "ViewRacksController.h"
+
+@interface ViewRacksController ()
+
+@end
+
+@implementation ViewRacksController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
