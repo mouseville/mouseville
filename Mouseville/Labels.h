99991399c0d3abcc8df0b7a6f93ae25044939d36
abhang <abhangs@gmail.com>
2014-04-12 22:56:11 -0400

    Added new Label Class

--- Mouseville/Labels.h
+++ Mouseville/Labels.h
@@ -2,7 +2,7 @@
 //  Labels.h
 //  Mouseville
 //
-//  Created by abhang on 4/6/14.
+//  Created by abhang on 4/12/14.
 //  Copyright (c) 2014 CapstoneProject. All rights reserved.
 //
 
@@ -13,7 +13,6 @@
 
 @interface Labels : NSManagedObject
 
-@property (nonatomic, retain) NSNumber * label_id;
 @property (nonatomic, retain) NSString * label_name;
 @property (nonatomic, retain) CageDetails *cageDetails;
 @property (nonatomic, retain) RackDetails *rackDetails;
--- Mouseville/Labels.m
+++ Mouseville/Labels.m
@@ -2,7 +2,7 @@
 //  Labels.m
 //  Mouseville
 //
-//  Created by abhang on 4/6/14.
+//  Created by abhang on 4/12/14.
 //  Copyright (c) 2014 CapstoneProject. All rights reserved.
 //
 
@@ -13,7 +13,6 @@
 
 @implementation Labels
 
-@dynamic label_id;
 @dynamic label_name;
 @dynamic cageDetails;
 @dynamic rackDetails;
