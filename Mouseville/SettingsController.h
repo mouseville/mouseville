a9be44385d70dcca8acbee6b5c984e0f85a75a66
nayan <nayankhodke@gmail.com>
2014-04-12 17:00:53 -0700

    Settings changes

--- Mouseville/SettingsController.h
+++ Mouseville/SettingsController.h
@@ -7,7 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "RackDetails.h"
 
 @interface SettingsController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *image1;
+
+@property (weak, nonatomic) IBOutlet UIImageView *image2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *image3;
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *image4;
+
+@property (weak, nonatomic) IBOutlet UIImageView *image5;
+
+@property (weak, nonatomic) IBOutlet UIImageView *image6;
+@property (weak, nonatomic) IBOutlet UIView *labelView;
+
+@property(weak, nonatomic) NSString *rackName;
+
+- (IBAction)saveLabels:(id)sender;
+
+
+- (IBAction)exportToCSV:(id)sender;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *label2;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *label3;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *label4;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *label1;
+
+@property (weak, nonatomic) IBOutlet UITextField *label5;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *label6;
+
+
 
 @end
